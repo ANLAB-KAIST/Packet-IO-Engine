5f8a03dd39c377c75b344a7b423e62a41270cd1a
Joongi Kim <joongi@an.kaist.ac.kr>
2012-09-06 19:14:14 +0900

    Generalized the affinity script.

--- driver/affinity.py
+++ driver/affinity.py
@@ -5,13 +5,38 @@ from __future__ import print_function, with_statement
 import os
 import sys
 import subprocess
+import re
 
-def execute(cmd):
+_exec_cache = {}
+
+def execute(cmd, cache=False):
+    global _exec_cache
+    if cache and cmd in _exec_cache:
+        return _exec_cache[cmd]
     try:
         proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-        return proc.communicate()[0]
+        result = proc.communicate()[0]
+        if cache:
+            _exec_cache[cmd] = result
+        return result
     except:
         return None
+
+def find_iface_node(name):
+    ifnode = -1
+    for line in execute('ethtool -i {0}'.format(ifname), cache=True).splitlines():
+        if line.startswith('bus-info:'):
+            bus_location = line.split(':', 1)[1].strip()
+            p1, p2, _ = bus_location.split(':')
+            bus_prefix = '{0}:{1:02x}'.format(p1, int(p2, 16) & 0xf0)
+            bus_affinity = execute('cat /sys/devices/pci{0}/pci_bus/{0}/cpuaffinity'.format(bus_prefix), cache=True).strip()
+            for node in range(num_nodes):
+                node_affinity = execute('cat /sys/devices/system/node/node{0}/cpumap'.format(node), cache=True).strip()
+                if node_affinity == bus_affinity:
+                    ifnode = node
+                    break
+    assert ifnode != -1
+    return ifnode
 
 if os.getuid() != 0:
     print('You must be root!', file=sys.stderr)
@@ -26,27 +51,14 @@ if len(sys.argv) < 3:
 ifname = sys.argv[1]
 num_devices = int(sys.argv[2])
 
-intrmap = execute('cat /proc/interrupts | grep %s-rx-' % ifname).strip().split('\n')
-#num_devices = int(execute('ifconfig -s | grep -c xge'))  # this gives incorrect value before "ifconfig up".
+intrmap = execute('cat /proc/interrupts | grep -i %s-rx-' % ifname).strip().split('\n')
 num_nodes = int(execute('cat /proc/cpuinfo | grep \'physical id\' | sort -u | wc -l'))
 
 for intr in intrmap:
     if intr:
-        irq = int(intr.split()[0][:-1])
-        queue = int(intr.split()[-1][-1])
-
-        if num_cpus == 4:
-            cpu = queue
-        elif num_cpus in (8, 12):
-            cpu = queue * 2
-            # assume that xge0 ~ xge(1/N-1) is in node 0,
-            #             xge(1/N) ~ xge(N) is in node 1.
-            #             where N is the number of devices.
-            if int(ifname[-1]) >= num_devices / num_nodes:
-                cpu += 1
-        else:
-            assert False, 'PacketShader supports only 4 or 8 core systems currently.'
-
+        irq = int(re.search(r'^\s*(\d+):', intr).group(1))
+        queue = int(re.search(r'-(\d+)$', intr).group(1))
+        cpu = (queue * num_nodes) + find_iface_node(ifname)
         print('echo %x > /proc/irq/%d/smp_affinity' % (1 << cpu, irq))
         execute('echo %x > /proc/irq/%d/smp_affinity' % (1 << cpu, irq))
     else:
