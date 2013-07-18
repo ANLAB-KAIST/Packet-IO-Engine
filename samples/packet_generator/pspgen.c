fd99a67a49ca9abb03d6a2d6270c80bc9c646c95
sangwook <suva.kaist.ac.kr>
2013-07-18 15:37:46 +0900

    Added pcap trace replaying function to packet generator sample program.

--- samples/packet_generator/pspgen.c
+++ samples/packet_generator/pspgen.c
@@ -30,13 +30,22 @@
 
 #include "psio.h"
 
+/* headers for examining pcap packet */
+#include <linux/tcp.h>
+#include <linux/icmp.h>
+#include <linux/icmpv6.h>
+#include <linux/in.h>
+
 #define MAX_PATH	260
 #define INET_ADDRSTRLEN		16
 #define INET6_ADDRSTRLEN	46
 #define ETH_EXTRA_BYTES	24  // preamble, padding bytes
 
+/* custom flag definitions to examine pcap packet */
+#define IPPROTO_IPv6_FRAG_CUSTOM 	44
+#define IPPROTO_ICMPv6_CUSTOM		58
+#define IPPROTO_OSPF_CUSTOM			89
 
-#define IPPROTO_UDP		17
 #define HTONS(n) (((((unsigned short)(n) & 0xFF)) << 8) | (((unsigned short)(n) & 0xFF00) >> 8))
 #define NTOHS(n) (((((unsigned short)(n) & 0xFF)) << 8) | (((unsigned short)(n) & 0xFF00) >> 8))
 
@@ -87,6 +96,43 @@ static uint8_t neighbor_ipv6addrs[PS_MAX_DEVICES][16];
 
 struct ps_handle handles[PS_MAX_CPUS];
 
+/* pcap_replaying: related variable & structs */
+static bool pcap_replaying = false;
+static bool pcap_looping = false;
+char pcap_filename[MAX_PATH] = {0, };
+char *pcap_alloc_file;
+size_t pcap_filesize;
+long pcap_num_pkts_total;
+long pcap_num_pkts_not_sent;		// pcap_replaying: temp
+uint32_t pcap_file_linktype;
+
+// from pcap format definition
+typedef struct pcap_file_header {
+	uint32_t magic;		/* magic number */
+	u_short version_major;  /* major version number */
+	u_short version_minor;	/* minor version number */
+	int32_t  thiszone;	/* GMT to local correction */
+	uint32_t sigfigs;	/* accuracy of timestamps */
+	uint32_t snaplen;	/* max length of captured packets, in octets */
+	uint32_t linktype;	/* data link type */
+} pcap_file_header_t;
+
+typedef struct pcap_pkthdr {
+	uint32_t ts_sec;	/* time stamp */
+	uint32_t ts_usec;	/* timestamp microseconds */
+	uint32_t caplen;	/* number of octets of packet saved in file */
+	uint32_t len;		/* actual length of packet */
+} pcap_pkthdr_t;
+
+typedef struct pcap_pkt_info {
+    size_t offset_pkt_content;  /* offset of captured packet in pcap file */
+    int caplen;            /* actual captured length of packet */
+    int len;               /* real length of packet on the link */
+} pcap_pkt_info_t;
+pcap_pkt_info_t *pcap_pkt_info_arr;
+long	pkt_info_arr_index;
+/* pcap_replaying: end */
+
 static inline uint64_t ps_rdtsc()
 {
     uint32_t xlo;
@@ -97,8 +143,65 @@ static inline uint64_t ps_rdtsc()
     return xlo | (((uint64_t) xhi) << 32);
 }
 
+/* function to check whether hyperthreading is enabled or not
+	copied from config.cc file in nShader */
+static void _cpuid(int i, uint32_t regs[4])
+{
+//#ifdef _WIN32
+//        __cpuid((int *)regs, (int)i);
+//#else
+        asm volatile
+        ("cpuid" : "=a" (regs[0]), "=b" (regs[1]),
+                   "=c" (regs[2]), "=d" (regs[3])
+	         : "a" (i), "c" (0));
+        // ECX is set to zero for CPUID function 4
+//#endif
+}
+
+bool check_hyperthreading(void) {
+	// Reference: http://stackoverflow.com/questions/2901694/
+	uint32_t regs[4];
+	// Get vendor string.
+	char vendor[12];
+	_cpuid(0, regs);
+	((uint32_t *)vendor)[0] = regs[1]; // EBX
+	((uint32_t *)vendor)[1] = regs[3]; // EDX
+	((uint32_t *)vendor)[2] = regs[2]; // ECX
+
+	// Get CPU features.
+	_cpuid(1, regs);
+	unsigned features = regs[3]; // EDX
+
+	// Get logical core count per CPU.
+	_cpuid(1, regs);
+	unsigned logical = (regs[1] >> 16) & 0xff; // EBX[23:16]
+	unsigned cores = logical;
+	if (!strncmp(vendor, "GenuineIntel", 12)) {
+		// Get DCP cache info
+		_cpuid(4, regs);
+		cores = ((regs[0] >> 26) & 0x3f) + 1; // EAX[31:26] + 1
+	} else if (!strncmp(vendor, "AuthenticAMD", 12)) {
+		// Get NC: Number of CPU cores - 1
+		_cpuid(0x80000008, regs);
+		cores = ((unsigned)(regs[2] & 0xff)) + 1; // ECX[7:0] + 1
+	}
+
+	// Detect hyper-threads.
+   bool has_hyperthreads = (features & (1 << 28)) && (cores < logical);
+   return has_hyperthreads;
+}
+///
+
 int ps_get_num_cpus() {
-	return sysconf(_SC_NPROCESSORS_ONLN);
+	// Assuming there is 2 hyper thread per physical CPU core..
+	long num_physical_cpu = sysconf(_SC_NPROCESSORS_ONLN);
+	int hyper = check_hyperthreading();
+	if (hyper) {
+		num_physical_cpu /= 2;	
+	}
+	
+	//printf("# of cpus: %d, hyperthreading: %d\n", num_physical_cpu, hyper);
+	return num_physical_cpu;
 }
 
 int ps_bind_cpu(int cpu) {
@@ -163,6 +266,71 @@ bool ps_in_samenode(int cpu, int ifindex)
 	return cpu_node == if_node;
 }
 
+/// pcap_replaying	
+void preprocess_pcap_file() {
+	FILE *file;
+	size_t read_size;
+	size_t offset = 0;
+	pcap_pkthdr_t *captured_pkt_hdr;
+	pcap_pkt_info_t *pkt_info;
+
+	pcap_num_pkts_total = 0;
+	long index = 0;
+	
+	printf("Now preprocessing pcap file..\n");
+
+	file = fopen(pcap_filename, "r");
+	if (file == NULL) {
+		fprintf(stderr, "Cannot open the pcap file \"%s\".\n", pcap_filename);
+		exit(1);
+	}
+
+	fseek(file, 0, SEEK_END);
+	pcap_filesize = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	pcap_alloc_file = (char*)malloc(pcap_filesize);
+	read_size = fread(pcap_alloc_file, pcap_filesize, 1, file);
+
+	if (read_size == -1) {
+		fprintf(stderr, "Failed to read pcap file \"%s\".\n", pcap_filename);
+		exit(1);
+	}
+
+    pcap_file_header_t *pcap_file_hdr;
+    pcap_file_hdr = (pcap_file_header_t *)pcap_alloc_file;
+    printf("Link type of packet trace: %u\n", pcap_file_hdr->linktype);
+    pcap_file_linktype = pcap_file_hdr->linktype;
+
+	// 1. look through pcap file & count whole number of packets
+	offset += sizeof(pcap_file_header_t);
+	while (offset < pcap_filesize) {
+		captured_pkt_hdr = (pcap_pkthdr_t*) (pcap_alloc_file + offset);
+		//printf("Packet #%d captured length: %d\n", pcap_num_pkts_total+1, captured_pkt_hdr->caplen);
+		offset += sizeof(pcap_pkthdr_t);
+		offset += captured_pkt_hdr->caplen;
+		pcap_num_pkts_total++;	
+	}
+
+	// 2. alloc packet info array
+	pcap_pkt_info_arr = (pcap_pkt_info_t*) malloc(pcap_num_pkts_total * sizeof(pcap_pkt_info_t));
+
+	// 3. set packet info into array
+	offset = sizeof(pcap_file_header_t);
+	while (offset < pcap_filesize) {
+		captured_pkt_hdr = (pcap_pkthdr_t*) (pcap_alloc_file + offset);
+		//printf("Packet #%d captured length: %d\n", pcap_num_pkts_total+1, captured_pkt_hdr->caplen);
+		offset += sizeof(pcap_pkthdr_t);
+		pkt_info = &pcap_pkt_info_arr[index];
+		pkt_info->offset_pkt_content = offset;
+		pkt_info->caplen = captured_pkt_hdr->caplen;
+		pkt_info->len	= captured_pkt_hdr->len;
+		offset += captured_pkt_hdr->caplen;
+		index++;	
+	}
+
+	printf("File size: %zu, number of packet: %ld\n", pcap_filesize, pcap_num_pkts_total);
+}
 
 void done()
 {
@@ -180,10 +348,16 @@ void done()
 		total_tx_packets += handle->tx_packets[ifindex];
 	}
 
+//	if (pcap_replaying) {
+//		printf("CPU#%d: current pkt info index: %ld\n", my_cpu, pkt_info_arr_index);
+//	}
 	printf("----------\n");
 	printf("CPU %d: total %ld packets transmitted\n", 
 			my_cpu, total_tx_packets);
-	
+	if (pcap_replaying) {
+		printf("CPU %d: total %ld packets not transmitted due to TX drop\n", my_cpu, pcap_num_pkts_not_sent);	// pcap_replaying 
+	}
+
 	for (i = 0; i < num_devices_registered; i++) {
 		char *dev = devices[devices_registered[i]].name;
 		ifindex = devices_registered[i];
@@ -473,6 +647,47 @@ void build_packet_v6(char *buf, int size, uint64_t *seed)
 	memset(content, 0xee, 1);  /* To indicate the beginning of packet content area. */
 }
 
+/// pcap_replaying
+void build_packet_from_pcap(char *buf, char* packet, int captured_size, int actual_size) {
+    // Copy the whole captured pcap packet.
+    // It's okay because currently we only use ethernet address in routing, which is overwritten after packet is built.
+    size_t filled_size = 0;
+
+    if (pcap_file_linktype == 1) /* LINKTYPE_ETHERNET */ 
+    {
+        memcpy(buf, packet, captured_size);
+        filled_size = captured_size;
+    }
+    else if (pcap_file_linktype == 101) /* LINKTYPE_RAW */
+    {
+        // Just to check whether raw packet is IPv4 or IPv6.
+        // It is okay because the version field of IPv4 & IPv6 is in same position.
+        // XXX: This code only handles IPv4 & IPv6 packet as L3 packet.
+        struct iphdr *l3_header = (struct iphdr *)packet;
+        struct ethhdr *eth = (struct ethhdr *) buf;
+
+        if (l3_header->version == 4) {
+            eth->h_proto = HTONS(ETH_P_IP);
+        }
+        else if (l3_header->version == 6) {
+            eth->h_proto = HTONS(ETH_P_IPV6);
+        }
+        memcpy(buf + sizeof(*eth), packet, captured_size);
+        filled_size = sizeof(*eth) + captured_size;
+    }
+    else {
+        printf("Linktype %d of pcap file is unhandled currently.\n", pcap_file_linktype);
+        exit(1); 
+    }
+
+    if (filled_size < actual_size) {
+        // Fill the rest of packet with a magic number 0xf0.
+        memset(buf + filled_size, 0xf0, actual_size - filled_size);
+        memset(buf + filled_size, 0xee, 1); // Indicating the beginning of packet content area.
+    }
+}
+///
+
 #define MAX_FLOWS 1024
 
 void send_packets(long packets, 
@@ -497,6 +712,11 @@ void send_packets(long packets,
 	if (num_flows == 0)
 		seed = time(NULL) + my_cpu;
 
+	/// pcap_replaying
+	pkt_info_arr_index = my_cpu;
+	pcap_num_pkts_not_sent = 0;
+	///
+	
 	// NOTE: If the num_flows option is used, the flow is generated
 	// with maximum sized packets and those sizes are cut randomly when
 	// filling the output chunk.
@@ -565,6 +785,7 @@ void send_packets(long packets,
 				chunk.cnt = chunk_size;
 
 			// TODO: implement throughput regulation for random sized packets.
+			/// pcap_replaying: TODO: handle throughput regulation for pcap replaying
 			if (offered_throughput > 0) {
 				unsigned need_to_send_bytes = check_rate(i);
 				if (need_to_send_bytes < 0) {
@@ -582,29 +803,53 @@ void send_packets(long packets,
 			}
 
 			/* Fill the chunk with packets generated. */
-			int offset = 0;
+			size_t offset = 0;	
 			for (j = 0; j < chunk.cnt; j++) {
 				int cur_pkt_size;
-				if (min_packet_size < packet_size)
-					cur_pkt_size = random() % (packet_size - min_packet_size) + min_packet_size;
-				else
-					cur_pkt_size = packet_size;
-				chunk.info[j].len = cur_pkt_size;
-				chunk.info[j].offset = offset;
-				offset = PS_ALIGN(offset + cur_pkt_size, 64);
 
-				if (num_flows == 0) {
-					if (ip_version == 4) {
-						build_packet(chunk.buf + chunk.info[j].offset,
-							     cur_pkt_size, &seed);
-					} else {
-						build_packet_v6(chunk.buf + chunk.info[j].offset,
-							cur_pkt_size, &seed);
+				/// pcap_replaying
+				/* TODO: limit program option.. "-v"(ip version) and "-f"(# of flows) options can't be used 
+						with pcap replay at the same time. */
+				if (pcap_replaying) {
+					// cur_pkt_size is determined from pcap file
+					pcap_pkt_info_t *packet;
+					packet = &pcap_pkt_info_arr[pkt_info_arr_index];
+					cur_pkt_size = packet->len;
+					chunk.info[j].offset = offset;
+					chunk.info[j].len = cur_pkt_size;
+				
+                    // If you want to control specific field of packet to be sent.. 
+					build_packet_from_pcap(chunk.buf + chunk.info[j].offset, pcap_alloc_file + packet->offset_pkt_content, packet->caplen, packet->len);
+					
+                    if (pkt_info_arr_index < (pcap_num_pkts_total-1-num_cpus)) {
+						pkt_info_arr_index += num_cpus;
 					}
-				} else {
-					memcpy_aligned(chunk.buf + chunk.info[j].offset, 
-						packet[(next_flow[i] + j) % num_flows], 
-						cur_pkt_size);
+					offset = PS_ALIGN(offset + cur_pkt_size, 64);
+					//printf("pcap_replaying: captured length:%d, pkt length:%d, offset:%d\n", header.caplen, header.len, offset);
+				}
+				///
+				else { 
+					if (min_packet_size < packet_size)
+						cur_pkt_size = random() % (packet_size - min_packet_size) + min_packet_size;
+					else
+						cur_pkt_size = packet_size;
+					chunk.info[j].len = cur_pkt_size;
+					chunk.info[j].offset = offset;
+					offset = PS_ALIGN(offset + cur_pkt_size, 64);
+
+					if (num_flows == 0) {
+						if (ip_version == 4) {
+							build_packet(chunk.buf + chunk.info[j].offset,
+									 cur_pkt_size, &seed);
+						} else {
+							build_packet_v6(chunk.buf + chunk.info[j].offset,
+								cur_pkt_size, &seed);
+						}
+					} else {
+						memcpy_aligned(chunk.buf + chunk.info[j].offset, 
+							packet[(next_flow[i] + j) % num_flows], 
+							cur_pkt_size);
+					}
 				}
 
 				/* Write the src/dest ethernet address corresponding to the
@@ -638,9 +883,16 @@ void send_packets(long packets,
 			}
 
 			ret = ps_send_chunk(handle, &chunk);
-			if (ret < 0)
+			if (ret < 0) {
 				perror("ps_send_chunk");
+				printf("arr_index: %ld\n", pkt_info_arr_index);// pcap_replaying
+			}
 			assert(ret >= 0);
+
+			/// pcap_replaying: checking the number of packets not sent
+			pcap_num_pkts_not_sent += chunk.cnt - ret;
+			// TODO: re-try to send remaining packets	
+			///
 
 			update_stats(handle);
 			sent += ret;
@@ -648,6 +900,17 @@ void send_packets(long packets,
 			if (latency_measure) {
 				for (j = 0; j < loop_count; j++)
 					receive_packets(handle, &chunk, packets <= sent);
+			}
+
+			if ( (pcap_replaying) && (pkt_info_arr_index >= (pcap_num_pkts_total-1-num_cpus))) {
+				if (pcap_looping) {
+					sent = 0;
+					pkt_info_arr_index = my_cpu;
+				}
+				else {
+					printf("CPU#%d: End of pcap file\n", my_cpu);
+					done();
+				}
 			}
 
 			if (packets <= sent)
@@ -681,6 +944,10 @@ void print_usage(char *program)
 			"[--neighbor-conf <neighbor config file>] "
 			"-i all|dev1 [-i dev2] ...\n",
 			program);
+
+	/// pcap_replaying
+	fprintf(stderr, "Or, to replay pcap file: -i all|dev1 [-i dev2] ... --pcap <pcap_file_name> [--loop]");
+	///
 
 	fprintf(stderr, "  default <num_packets> is 0. (0 = infinite)\n");
 	fprintf(stderr, "    (note: <num_packets> is a per-cpu value.)\n");
@@ -801,6 +1068,15 @@ int main(int argc, char **argv)
 		} else if (!strcmp(argv[i], "--neighbor-conf")) {
 			strncpy(neighbor_conf_filename, argv[i + 1], MAX_PATH);
 			assert(strnlen(neighbor_conf_filename, MAX_PATH) > 0);
+		/// pcap_replaying
+		} else if (!strcmp(argv[i], "--loop")) {
+			pcap_looping = true;
+			i--;
+		} else if (!strcmp(argv[i], "--pcap")) {
+			strncpy(pcap_filename, argv[i + 1], MAX_PATH);
+			assert(strnlen(pcap_filename, MAX_PATH) > 0);
+			pcap_replaying = true;
+		///
 		} else
 			print_usage(argv[0]);
 	}
@@ -814,6 +1090,18 @@ int main(int argc, char **argv)
 		fprintf(stderr, "No devices registered!\n");
 		print_usage(argv[0]);
 	}
+
+	/// pcap_replaying
+	if (pcap_replaying && num_flows != 0) {
+		fprintf(stderr, "Number of flows cannot be set when replaying pcap file.\n");
+		exit(1);
+	}
+	if (pcap_replaying && offered_throughput > 0) {
+		fprintf(stderr, "Throughput regulation is not supported when replaying pcap file.\n");
+		exit(1);
+	}
+	// TODO: restrict option (some options can't be used with -pcap)
+	///
 
 	/* Read neighbor configuration from file.
 	 * We currently do not use IP addresses since experimenting a router uses random IP
@@ -856,6 +1144,12 @@ int main(int argc, char **argv)
 	/* TODO: make it possible arbitrary source-destination interface mappings. */
 	assert(num_neighbors >= num_devices_registered);
 
+	/// pcap_replaying: whole pcap file is allocated to memory & indexed before replaying starts
+	if (pcap_replaying) {
+		preprocess_pcap_file();
+	}
+	///
+ 
 	/* Show the configuration. */
 
 	printf("# of CPUs = %d\n", num_cpus);
@@ -911,6 +1205,15 @@ int main(int argc, char **argv)
 		if (ret == -1 && errno == ECHILD)
 			break;
 	}
+
+	/// pcap_replaying
+	if (pcap_replaying) {
+		if (pcap_alloc_file != 0) {
+			free(pcap_alloc_file);
+			pcap_alloc_file = 0;
+		}
+	}
+	///
 
 	assert(gettimeofday(&end, NULL) == 0);
 
